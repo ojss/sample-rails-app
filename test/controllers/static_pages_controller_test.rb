b8e59ee24717723ff1fae559a3e9e84c80de2b55
Ojas Shirekar <oshirekar@hotmail.com>
2015-12-08 00:45:54 +0530

    added provide titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,14 +4,17 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Sample App"
   end
   test "should get about" do
     get :about
-    assert :success
+    assert_response :success
+    assert_select "title", "About | Sample App"
   end
 end
